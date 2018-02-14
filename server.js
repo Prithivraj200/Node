ac003fa4c79e402d1ec86ea82c9869a6802c575e
Prithivraj200 <prethive200@gmail.com>
2018-02-14 17:32:34 +0530

    v1.0

---
+++ server.js
@@ -0,0 +1,48 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+var app=express();
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/web'));
+app.use((req,res,next)=>{
+	const now=new Date().toString();
+	const log=`${now} ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log',log+"\n",(err)=>{
+		if(err) console.log("Not logged !")
+	})
+	next();
+});
+// app.use((req,res,next)=>{
+// 	res.render('maintenance.hbs');
+// })
+hbs.registerPartials(__dirname+'/views/partials');
+
+hbs.registerHelper('date',()=>{
+	return new Date().getFullYear()
+})
+
+hbs.registerHelper('transform',(text)=>{
+	return text.toUpperCase();
+})
+
+const profile={
+	           name:"Prithivraj",
+		       aoi:'Ionic'
+		      }
+const data=`I'm ${profile.name} with area of Interest in ${profile.aoi}`;
+
+app.get('/',(req,res)=>{
+	res.render('home.hbs',{
+		message:'Welcome Home Baby!'
+	});
+})
+
+app.get('/info',(req,res)=>{
+	res.render('info.hbs',{
+		message:data
+	});
+})
+
+app.listen(4000);
